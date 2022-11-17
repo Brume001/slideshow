7c89a0b9077664fe43d36eff318086624462d16f
Desmond <114571991+Brume001@users.noreply.github.com>
2022-11-17 04:38:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+const buttons = document.querySelectorAll("[data-carousel-button]")
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1
+        const slides = button.closest("[data-carousel")
+        .querySelector("[data-slides")
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1}
-    if (n < 1) { slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("active", "")
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += "active";
+        const activeSlide = slides.querySelector("[data-active]")
+        let newIndex = [...slides.children].indexOf(activeSlide) + offset
+        if (newIndex < 0) newIndex = slides.children.length - 1
+        if (newIndex >= slides.children.length) newIndex = 0
 
-}
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.datasetactive
+    })
+})
